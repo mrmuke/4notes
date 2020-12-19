2736ba2fc2c7a249e8a32818c2e23a981f6c6fc0
mrmuke <m.xingster@gmail.com>
2020-12-19 12:22:50 -0600

    training model and gui

--- predicting.py
+++ predicting.py
@@ -6,24 +6,43 @@ from keras.layers import Dense
 from keras.layers import Dropout
 from keras.layers import LSTM
 from keras.layers import Activation
+from keras.callbacks import ModelCheckpoint
+from keras.layers import BatchNormalization as BatchNorm
+import os
 
-midi = converter.parse("data/mozart.mid")
+
+#training the model
+def train_model(model, network_input,network_output):
+    filepath = "weights-improvement-{epoch:02d}-{loss:.4f}-bigger.hdf5"    
+    #save weights so we don't have to wrorry about losing them
+    checkpoint = ModelCheckpoint(
+        filepath, monitor='loss', 
+        verbose=0,        
+        save_best_only=True,        
+        mode='min' 
+    )     
+    callbacks_list = [checkpoint]      
+    model.fit(network_input, network_output, epochs=200, batch_size=128, callbacks=callbacks_list)
+ #get all notes from midi files - mozart bach beethoven chopin tchaikovsky
+
 notes = []
+for filename in os.listdir("data"):
+    midi = converter.parse("data/"+filename) 
+    print(filename)
+    notes_to_parse = None
+    #parsing notes from midi files
+    try:
+        s2 = instrument.partitionByInstrument(midi)
+        notes_to_parse = s2.parts[0].recurse() 
+    except:
+        notes_to_parse = midi.flat.notes
+    for element in notes_to_parse:
+        if isinstance(element, note.Note):
+            notes.append(str(element.pitch))
+        elif isinstance(element, chord.Chord):
+            notes.append('.'.join(str(n) for n in element.normalOrder))
 
-notes_to_parse = None
 
-try:
-    s2 = instrument.partitionByInstrument(midi)
-    notes_to_parse = s2.parts[0].recurse() 
-except:
-    notes_to_parse = midi.flat.notes
-print(notes_to_parse)
-for element in notes_to_parse:
-    if isinstance(element, note.Note):
-        notes.append(str(element.pitch))
-    elif isinstance(element, chord.Chord):
-        notes.append('.'.join(str(n) for n in element.normalOrder))
-print(notes)
 #how many input notes to determine output (use dice and other method?)
 sequence_length = 100
 pitchnames = sorted(set(item for item in notes))
@@ -41,23 +60,35 @@ network_input = numpy.reshape(network_input, (len(network_input), sequence_lengt
 network_input = network_input / float(len(set(notes)))
 network_output = np_utils.to_categorical(network_output)
 
-#create network
+#create network and structure of it
+#play around iwth number of each see if make better predictions
+#number in parameter as number of nodes the layer should have 
+
 model = Sequential()
 model.add(LSTM(
-    256,
+    512,
     input_shape=(network_input.shape[1], network_input.shape[2]),
+    recurrent_dropout=0.3,
     return_sequences=True
 ))
-#number in parameter as number of nodes 
-model.add(Dropout(0.3))
-model.add(LSTM(512, return_sequences=True))
-model.add(Dropout(0.3))
 #LSTM can take sequence input and return sequence output
-model.add(LSTM(256))
-model.add(Dense(256))
-#prevent overfitting
+
+model.add(LSTM(512, return_sequences=True, recurrent_dropout=0.3,))
+model.add(LSTM(512))
+model.add(BatchNorm())
+#prevent overfitting (fraction of input nodes that should be dropped out)
+
 model.add(Dropout(0.3))
-model.add(Dense(n_vocab))
+model.add(Dense(256))
+model.add(Activation('relu'))
+#speed up
+model.add(BatchNorm())
+model.add(Dropout(0.3))
+#Dense determines where the corresponding output node is to input node
+
+model.add(Dense(len(set(notes))))
 #determine the output of a node
+
 model.add(Activation('softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+train_model(model,network_input,network_output)
