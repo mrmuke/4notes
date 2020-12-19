a196b0cad86126ae9ce6a70959d1129943c37d75
mrmuke <m.xingster@gmail.com>
2020-12-19 16:28:48 -0600

    genetic algo

--- predict.py
+++ predict.py
@@ -29,7 +29,7 @@ def create_network(network_input, pitches):
     model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
 
     # Load the weights to each node
-    model.load_weights('weights-improvement-26-3.6319-bigger.hdf5')
+    model.load_weights('weights-improvement-27-3.5857-bigger.hdf5')
 
     return model
 
