aa323697f40b95705dc5c1ac3562f346a1730b65
mrmuke <m.xingster@gmail.com>
2020-12-19 00:38:20 -0600

    identifying notes

---
+++ ga.py
@@ -0,0 +1,55 @@
+from random import choices, randint, randrange, random, sample
+from typing import List, Optional, Callable, Tuple
+
+Genome = List[int]
+Population = List[Genome]
+FitnessFunc = Callable[[Genome], int]
+def generate_genome(length: int) -> Genome:
+    return choices([0, 1], k=length)
+
+def single_point_crossover(a: Genome, b: Genome) -> Tuple[Genome, Genome]:
+    if len(a) != len(b):
+        raise ValueError("Genomes a and b must be of same length")
+
+    length = len(a)
+    if length < 2:
+        return a, b
+
+    p = randint(1, length - 1)
+    return a[0:p] + b[p:], b[0:p] + a[p:]
+
+
+def mutation(genome: Genome, num: int = 1, probability: float = 0.5) -> Genome:
+    for _ in range(num):
+        index = randrange(len(genome))
+        genome[index] = genome[index] if random() > probability else abs(genome[index] - 1)
+    return genome
+
+
+
+def selection_pair(population: Population, fitness_func: FitnessFunc) -> Population:
+    return sample(
+        population=generate_weighted_distribution(population, fitness_func),
+        k=2
+    )
+
+
+def generate_weighted_distribution(population: Population, fitness_func: FitnessFunc) -> Population:
+    result = []
+
+    for gene in population:
+        result += [gene] * int(fitness_func(gene)+1)
+
+    return result
+
+
+#genome_1 = generate_genome(5)
+#genome_2=generate_genome(5)
+#print(genome_1)
+#print(genome_2)
+#print(single_point_crossover(genome_1,genome_2))
+#print(mutation(genome_1))
+#population = [genome_1,genome_2]
+#def fitness(genome:Genome):
+#    return 0
+#print(selection_pair(population,fitness))
